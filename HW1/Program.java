ac240e82e5df3618113426ce6689ca5898875eb5
Long <long1685@colorado.edu>
2019-01-31 19:25:58 -0700

    Added feature for counting number of shapes in database

--- HW1/Program.java
+++ HW1/Program.java
@@ -4,6 +4,7 @@ public class Program{
     public static void main(String[] args){
         //sets up pseudo database of shapes
         ArrayList<Shape> database = new ArrayList<Shape>();
+        
         database.add(new Square());
         database.add(new Triangle());
         database.add(new Triangle());
@@ -17,6 +18,9 @@ public class Program{
         database.add(new Square());
         database.add(new Triangle());
         database.add(new Square());
+
+        int numShapes = database.size();
+        System.out.println("Database contains " + numShapes + " shapes");
 
         for (Shape shape : database){
             shape.display();
