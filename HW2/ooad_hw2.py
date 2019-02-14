c9c613fa6f1598b613cfaead52eb9247fd41c7c1
Danny-Nguyen <Danny-Nguyen@users.noreply.github.com>
2019-02-14 14:06:32 -0700

    Update Python Shapes

--- HW2/ooad_hw2.py
+++ HW2/ooad_hw2.py
@@ -1,20 +1,20 @@
-class Shape:
+class Shape: #Abstract Shape Class
 	def display(self): pass
 
-class Circle(Shape):
+class Circle(Shape): #Circle is a subclass of Shape and prints out what it is in its overridden display method.
 	def display(self):
 		print("Circle")
 
-class Square(Shape):
+class Square(Shape): #Square is a subclass of Shape and prints out what it is in its overridden display method.
 	def display(self):
 		print("Square")
 
-class Triangle(Shape):
+class Triangle(Shape): #Triangle is a subclass of Shape and prints out what it is in its overridden display method.
 	def display(self):
 		print("Triangle")
 
 def main():
-	database = []
+	database = [] #database list will act as a database that stores shape objects
 	
 	database.append(Square())
 	database.append(Triangle())
@@ -30,8 +30,8 @@ def main():
 	database.append(Circle())
 
 
-	print("There are " + str(len(database)) + " shapes in the database.")
-	for shape in database:
+	print("There are " + str(len(database)) + " shapes in the database.") #print out number of shapes in database
+	for shape in database: #each shape has the ability to display itself
 		shape.display()
 
 
