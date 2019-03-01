31bf926de1ca03e9e28b15afb3d75cfea92781f8
Danny-Nguyen <Danny-Nguyen@users.noreply.github.com>
2019-03-01 02:40:17 -0700

    finished hw3

--- HW3/ooad_hw3.py
+++ HW3/ooad_hw3.py
@@ -6,12 +6,8 @@ class Customer(metaclass = ABCMeta): #Abstract customer class
 	def __init__(self, name, typeOf):
 		self.name = name #unique customer name
 		self.typeOf = typeOf #one of three customer types
-		mintools = 0
-		maxtools = 3
-		maxrent = 3
-		rents = 0
-		mintime = 0
-		maxtime = 7
+		tool = 0 #number of tools available to rent for customer type
+		time = 0 #number of nights the customer type may rent their tools for
 		pass
 	@property
 	@abstractmethod
@@ -31,11 +27,15 @@ class Casual(Customer): #Derived Casual customer class
 		tool = randint(1, 2)
 		toolstaken = [] # List to track tools
 		for i in range(tool):
-			toolstaken.append(tools.pop(0)) # Append it to the tracking list
-			return (toolstaken, tools)
+			if (tools == []): # If the tools catalog is empty report that there is nothing to rent
+				print("Nothing to rent right now")
+			else:
+				toolstaken.append(tools.pop(0)) # Append it to the tracking list
+			return (toolstaken, tools) #Return tuple of tools rented by customer and new catalog of tools
 	def returns(self, toolsRented, tools):
-		for i in range (time):
+		for i in range (len(toolsRented)):
 			tools.append(toolsRented[i]) # Return the tool back to the catalog
+		return toolsRented
 
 class Regular(Customer): #Derived Regular customer class
 	def __init__(self, name):
@@ -48,30 +48,38 @@ class Regular(Customer): #Derived Regular customer class
 		tool = randint(1, 3)
 		toolstaken = [] # List to track tools
 		for i in range(tool):
-			toolstaken.append(tools.pop(0)) # Append it to the tracking list
-		return (toolstaken, tools)
+			if (tools == []): # If the tools catalog is empty report that there is nothing to rent
+				print("Nothing to rent right now")
+			else:
+				toolstaken.append(tools.pop(0)) # Append it to the tracking list
+		return (toolstaken, tools) #Return tuple of tools rented by customer and new catalog of tools
 	def returns(self, toolsRented, tools):
-		for i in range (time):
-			tools.append(toolsRented[i])
+		for i in range (len(toolsRented)):
+			tools.append(toolsRented[i]) # Return the tool back to the catalog
+		return toolsRented
 
 class Business(Customer): #Derived Business customer class
 	def __init__(self, name):
 		self.name = name
 		self.typeOf = "Business"
-		self.time = 7
-		self.tool = 3
+		self.time = 7 #number of time rented
+		self.tool = 3 #number of tools rented
 
 	def rentals(self, tools):
 		tool = 3
-		toolstaken = []
+		toolstaken = [] # List to track tools
 		for i in range(tool):
-			toolstaken.append(tools.pop(0)) # Append it to the tracking list
-		return (toolstaken, tools)
+			if (tools == []): # If the tools catalog is empty report that there is nothing to rent
+				print("Nothing to rent right now")
+			else:
+				toolstaken.append(tools.pop(0)) # Append it to the tracking list
+		return (toolstaken, tools) #Return tuple of tools rented by customer and new catalog of tools
 	def returns(self, toolsRented, tools):
-		for i in range (time):
+		for i in range (len(toolsRented)):
 			tools.append(toolsRented[i]) # Return the tool back to the catalog
+		return toolsRented
 
-class customerFactory:
+class customerFactory: #Factory for encapsulating creation of customer objects
 	def Casual(name): # Create casual customer
 		return(Casual(name))
 	def Regular(name): # Create regular customer
@@ -82,13 +90,13 @@ class customerFactory:
 		customers = []
 		num = 0
 		for i in range(10):
-			randint = np.random.choice(3, 1)
+			randint = np.random.choice(3, 1) #Create customer type based on random number
 			if randint == 0:
-				customers.append(Casual("Customer " +str(num)))
+				customers.append(Casual("Customer " +str(num))) #Give the customer a unique name
 			elif randint == 1:
-				customers.append(Regular("Customer " +str(num)))
+				customers.append(Regular("Customer " +str(num))) #Give the customer a unique name
 			else:
-				customers.append(Business("Customer " +str(num)))
+				customers.append(Business("Customer " +str(num))) #Give the customer a unique name
 			num += 1
 		return customers
 
@@ -96,47 +104,47 @@ class Tool(metaclass = ABCMeta):
 	def __init__(self, name, typeOf):
 		self.name = name # Unique tool name
 		self.typeOf = typeOf # One of 5 tool types
-		self.returnDay = 0
+		self.returnDay = 0 #How long until the tool should be returned
 	
 	def cost(self): # Cost of renting tool
 		pass
 
 class Painting(Tool):
 	def __init__(self, name, typeOf):
-		Tool.__init__(self, name, typeOf)
+		Tool.__init__(self, name, typeOf) # Override variable values from abstract super class
 		self.returnDay
 	def cost(self):
-		return 5
+		return 5 # Specific cost of tool category
 
 class Concrete(Tool):
 	def __init__(self, name, typeOf):
-		Tool.__init__(self, name, typeOf)
+		Tool.__init__(self, name, typeOf) # Override variable values from abstract super class
 		self.returnDay
 	def cost(self):
-		return 10
+		return 10 # Specific cost of tool category
 
 class Plumbing(Tool):
 	def __init__(self, name, typeOf):
-		Tool.__init__(self, name, typeOf)
+		Tool.__init__(self, name, typeOf) # Override variable values from abstract super class
 		self.returnDay
 	def cost(self):
-		return 7
+		return 7 # Specific cost of tool category
 
 class Woodwork(Tool):
 	def __init__(self, name, typeOf):
-		Tool.__init__(self, name, typeOf)
+		Tool.__init__(self, name, typeOf) # Override variable values from abstract super class
 		self.returnDay
 	def cost(self):
-		return 15
+		return 15 # Specific cost of tool category
 
 class Yardwork(Tool):
 	def __init__(self, name, typeOf):
-		Tool.__init__(self, name, typeOf)
+		Tool.__init__(self, name, typeOf) # Override variable values from abstract super class
 		self.returnDay
 	def cost(self):
-		return 12
+		return 12 # Specific cost of tool category
 
-class toolFactory():
+class toolFactory(): # Factory for encapsulating creation of tool objects
 	def creation(self):
 		tools = [] # Catalog 20 tools in store
 
@@ -145,69 +153,71 @@ class toolFactory():
 			tools.append(choice)
 
 		counter = 0
-		for tool, item in enumerate(tools):
+		for tool, item in enumerate(tools): # For each item in the tools array 
 			counter += 1
 			if item == 0:
-				tools[tool] = Painting("Tool-" +str(counter), "Painting")
+				tools[tool] = Painting("Tool-" +str(counter), "Painting") # Give the object a unique name
 			elif item == 1:
-				tools[tool] = Concrete("Tool-" +str(counter), "Concrete")
+				tools[tool] = Concrete("Tool-" +str(counter), "Concrete") # Give the object a unique name
 			elif item == 2:
-				tools[tool] = Plumbing("Tool-" +str(counter), "Plumbing")
+				tools[tool] = Plumbing("Tool-" +str(counter), "Plumbing") # Give the object a unique name
 			elif item == 3:
-				tools[tool] = Woodwork("Tool-" +str(counter), "Woodwork")
+				tools[tool] = Woodwork("Tool-" +str(counter), "Woodwork") # Give the object a unique name
 			else:
-				tools[tool] = Yardwork("Tool-" +str(counter), "Yardwork")
+				tools[tool] = Yardwork("Tool-" +str(counter), "Yardwork") # Give the object a unique name
 
 		return tools
 
 class Store:
 	def checkoutCustomer(self, customers, toolArray): # Method to allow customers to rent tools
-		rentaldata = []
+		rentaldata = [] # Store rental receipts for customers
 		revenue = 0 # Amount of money gained from renting tools
-		for i, j in enumerate(customers): 
+		for i, j in enumerate(customers): # Check if each customer may rent tools based on what is available. 
 
 			if (len(toolArray) > customers[i].tool): # If the catalog array has enough tools for the type of customer to rent then allow them to rent tools
-				rental = Rental() # Crete a rental for each purchase
-				rentaldata.append((rental.createRental(customers[i].rentals(toolArray)[0], customers[i].time)))
+				rental = Rental() # Create a rental for each purchase
+				createrent = (rental.createRental(customers[i].rentals(toolArray)[0], customers[i].time)) # Fill the rental with the tool objects and when to return them by
+				print("Renting - " + str(createrent)) 
+				rentaldata.append(createrent) # Add to store of rental receipts
 
 			else:
-				pass
-				#print("The requested amount of tools is not available.")
-		
-		for item, j in enumerate(rentaldata):
-			revenue += (rentaldata[item][1])
+				print("The requested amount of tools is not available.") # If there aren't enough tools for that customer print this statement
+
+		for item, j in enumerate(rentaldata): # For each rental receipt
+			revenue += (rentaldata[item][1]) # Sum the revenue gained from each customer
+
 		return (rentaldata, revenue)
-
-	def returnCustomer(self, customers, rentalArray):
-		pass
-
 
 class Rental:
 	def createRental(self, toolsRented, nights): # Create a rental that contains tools rented and how many nights rented for
 		revenue = 0
-		rental = []
-		for item, j in enumerate(toolsRented):
-			rental.append(toolsRented[item].name)
-			rental.append(toolsRented[item].typeOf)
-			revenue += toolsRented[item].cost()
+		rental = [] # Rental receipts are displayed as lists
+		for item, j in enumerate(toolsRented): # For each tool that a customer rents
+			rental.append(toolsRented[item].name) # Add the name of the tool to the rental receipt
+			rental.append(toolsRented[item].typeOf) # Add the type of the tool to the rental receipt
+			revenue += toolsRented[item].cost() # Add the total cost of the tools to the rental receipt
 
-		rental.append(nights)
+		rental.append(nights) # Add the number of nights before the customer must return the tools
 		return (rental, revenue)
 
 def simulate(): # Simulation function creates objects and factories
-	tools = toolFactory()
-	customers = customerFactory()
-	tools.creation()
-	customers.creation()
-	store = Store()
-	#store.checkoutCustomer(customers.creation(), tools.creation())
-	print("Daily revenue: " + str(store.checkoutCustomer(customers.creation(), tools.creation())[1]))
-	#print(store.checkoutCustomer(customers.creation(), tools.creation()))
+	days = [1, 2, 3, 4, 5, 6, 7] # Create days list to simulate days of the week
+	tools = toolFactory() # Create instance of factory
+	customers = customerFactory() # Create instance of factory
+	tools.creation() # Call method to create objects
+	customers.creation() # Call method to create objects
+	store = Store() # Create instance of store class
+	
+	for i in range(35): # Run simulation for 35 nights
+		print("Day - " + str(i)) # Print out what day the simulation is on
+		print("Total revenue: " + str(store.checkoutCustomer(customers.creation(), tools.creation())[1])) # Sum the total daily revenue 
+
+		if (days[i % len(days)] == customers.creation()[i % len(customers.creation())].time): # Checks if the customer's rental is out of time
+			customers.creation()[i % len(customers.creation())].returns(store.checkoutCustomer(customers.creation(), tools.creation()), tools.creation()) # Readd tools to the store's catalog 
+			print("Returning - " + str(customers.creation()[i % len(customers.creation())].returns(store.checkoutCustomer(customers.creation(), tools.creation()), tools.creation())))
 
 def main():
-	#simulate()
-	for i in range(35):
-		simulate()
+	simulate() # Call simulation method
 		
 if __name__ == '__main__':
 	main()
