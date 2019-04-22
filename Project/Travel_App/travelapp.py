98cc771d365fa0fb829892e508106ef17a0dccde
Long Nguyen <long.t.nguyen@colorado.edu>
2019-04-22 12:56:01 -0600

    setting up package structure

--- Project/Travel_App/travelapp.py
+++ Project/Travel_App/travelapp.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
+from forms import RegustrationForm, LoginForm
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = ''
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
-class User(db.Model):
+'''class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -13,7 +16,7 @@ class User(db.Model):
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
-
+'''
 
 @app.route("/")
 def home():
