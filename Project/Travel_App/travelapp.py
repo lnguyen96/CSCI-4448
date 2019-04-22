99c22db0804249461f399439dcc5bf8e4797c70b
Long Nguyen <long.t.nguyen@colorado.edu>
2019-04-21 19:46:21 -0600

    setup template for database

--- Project/Travel_App/travelapp.py
+++ Project/Travel_App/travelapp.py
@@ -1,5 +1,19 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
 
 @app.route("/")
 def home():
